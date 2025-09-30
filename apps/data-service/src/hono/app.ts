79f22cbaa1fc4d4430ac7918e41223c38de4798c
juanjuanzero <juanjuanzerojuan@gmail.com>
2025-09-30 06:13:11 -0400

    smart routing service: geobased routing

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,7 +1,25 @@
+import { getDestinationForCountry } from "@/helpers/routing-ops";
+import { getLink } from "@repo/data-ops/queries/links";
+import { cloudflareInfoSchema } from "@repo/data-ops/zod-schema/links";
 import {Hono} from "hono";
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
-    return c.json({message: 'Hello World!'});
+ 
+    const linkInfoFromDb = await getLink(c.req.param('id'));
+    if (!linkInfoFromDb) {
+        return c.json({ error: 'Link not found' }, 404);
+    }
+
+    const cfHeaders = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+    if (!cfHeaders.success) {
+        console.log(cfHeaders.error);
+        return c.json({ error: 'Error Parsing Cloudflare Headers' }, 400);
+    }
+
+    const headers = cfHeaders.data;
+    const destinations = getDestinationForCountry(linkInfoFromDb, headers.country);
+    return c.redirect(destinations
+    ) 
 })
