e1756dc331f01e6973dd0f326ffe646aa2e78748
juanjuanzero <juanjuanzerojuan@gmail.com>
2025-11-02 06:17:16 -0500

    building ai workflows

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,12 +1,23 @@
+import { aiDestinationChecker } from '@/helpers/ai-desitnation-checker';
 import { collectDestinationInfo } from '@/helpers/browser-render';
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, unknown> {
-        async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+	async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+		const collectedData = await step.do('Collect rendered destination page data', async () => {
+			return collectDestinationInfo(this.env, event.payload.destinationUrl);
+		});
 
-        const collectedData = await step.do("Collect rendered destination page data", async () => {
-            return collectDestinationInfo(this.env, event.payload.destinationUrl);
-        });
-        console.log(collectedData);
-    }
+		const aiStatus = await step.do(
+			'Use AI to check status of page',
+			{
+				retries: { limit: 0, delay: 0 },
+			},
+			async () => {
+				return await aiDestinationChecker(this.env, collectedData.bodyText);
+			}
+		);
+
+		console.log(collectedData);
+	}
 }
