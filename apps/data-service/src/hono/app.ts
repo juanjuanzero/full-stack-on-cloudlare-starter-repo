762a2f992c0ad66195419f997bdbd85b48676a96
juanjuanzero <juanjuanzero@juanjuanzeros-Mac-mini.local>
2026-01-08 06:23:03 -0500

    durable objects: building base class

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,37 +1,49 @@
-import { getDestinationForCountry, getRoutingDestinations } from "@/helpers/routing-ops";
-import { getLink } from "@repo/data-ops/queries/links";
-import { cloudflareInfoSchema } from "@repo/data-ops/zod-schema/links";
-import { LinkClickMessageType } from "@repo/data-ops/zod-schema/queue";
-import {Hono} from "hono";
+import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/routing-ops';
+import { getLink } from '@repo/data-ops/queries/links';
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
+import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
 
+App.get('/do/:name', async (c) => {
+	const name = c.req.param('name');
+	const doId = c.env.EVALUATION_SCHEDULER.idFromName(name);
+	const stub = c.env.EVALUATION_SCHEDULER.get(doId);
+	await stub.increment();
+	const count = await stub.getCount();
+	return c.json({
+		count,
+	});
+});
+
 App.get('/:id', async (c) => {
-    const id = c.req.param('id');
-    const linkInfoFromDb = await getRoutingDestinations(c.env, id); 
-    if (!linkInfoFromDb) {
-        return c.json({ error: 'Link not found' }, 404);
-    }
+	const id = c.req.param('id');
+	const linkInfoFromDb = await getRoutingDestinations(c.env, id);
+	if (!linkInfoFromDb) {
+		return c.json({ error: 'Link not found' }, 404);
+	}
 
-    const cfHeaders = cloudflareInfoSchema.safeParse(c.req.raw.cf);
-    if (!cfHeaders.success) {
-        console.log(cfHeaders.error);
-        return c.json({ error: 'Error Parsing Cloudflare Headers' }, 400);
-    }
+	const cfHeaders = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+	if (!cfHeaders.success) {
+		console.log(cfHeaders.error);
+		return c.json({ error: 'Error Parsing Cloudflare Headers' }, 400);
+	}
 
-    const headers = cfHeaders.data;
-    const destination = getDestinationForCountry(linkInfoFromDb, headers.country);
-    const queueMessage : LinkClickMessageType = {
-        type: "LINK_CLICK",
-        data: {
-            id: id,
-            country: headers.country,
-            destination: destination,
-            accountId: linkInfoFromDb.accountId,
-            latitude: headers.latitude,
-            longitude: headers.longitude,
-            timestamp: new Date().toISOString(),
-        }}
-    c.executionCtx.waitUntil(c.env.QUEUE.send(queueMessage));
-    return c.redirect(destination) 
-})
+	const headers = cfHeaders.data;
+	const destination = getDestinationForCountry(linkInfoFromDb, headers.country);
+	const queueMessage: LinkClickMessageType = {
+		type: 'LINK_CLICK',
+		data: {
+			id: id,
+			country: headers.country,
+			destination: destination,
+			accountId: linkInfoFromDb.accountId,
+			latitude: headers.latitude,
+			longitude: headers.longitude,
+			timestamp: new Date().toISOString(),
+		},
+	};
+	c.executionCtx.waitUntil(c.env.QUEUE.send(queueMessage));
+	return c.redirect(destination);
+});
--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -4,27 +4,27 @@ import { initDatabase } from '@repo/data-ops/database';
 import { QueueMessageSchema } from '@repo/data-ops/zod-schema/queue';
 import { handleLinkClick } from './queue-handlers/link-clicks';
 export { DestinationEvaluationWorkflow } from './workflows/destination-evaluation-workflow';
+export { EvaluationScheduler } from '@/durable-objects/evaluation-scheduler';
 
 export default class DataService extends WorkerEntrypoint<Env> {
-	constructor(ctx: ExecutionContext, env : Env) {
+	constructor(ctx: ExecutionContext, env: Env) {
 		initDatabase(env.DB);
 		super(ctx, env);
 	}
 
 	fetch(request: Request) {
-		return App.fetch(request, this.env, this.ctx); 
+		return App.fetch(request, this.env, this.ctx);
 	}
-	async queue(batch: MessageBatch<unknown>){
-		for (const message of batch.messages){
-			const parsedEvent = QueueMessageSchema.safeParse(message.body);	
-			if (parsedEvent.success){
-			
-			if (parsedEvent.data.type === "LINK_CLICK"){
-				await handleLinkClick(this.env, parsedEvent.data);
+	async queue(batch: MessageBatch<unknown>) {
+		for (const message of batch.messages) {
+			const parsedEvent = QueueMessageSchema.safeParse(message.body);
+			if (parsedEvent.success) {
+				if (parsedEvent.data.type === 'LINK_CLICK') {
+					await handleLinkClick(this.env, parsedEvent.data);
+				}
+			} else {
+				console.error('Failed to parse queue message:', parsedEvent.error);
 			}
-		} else {
-				console.error("Failed to parse queue message:", parsedEvent.error);
 		}
 	}
-}
 }
