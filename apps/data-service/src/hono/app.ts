36d6044557a2839f026605f741a480d45d6a9c17
juanjuanzero <juanjuanzero@mac.lan>
2026-01-12 06:04:41 -0500

    durable objects

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,10 +1,23 @@
-import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/routing-ops';
+import { captureLinkClickInBackground, getDestinationForCountry, getRoutingDestinations } from '@/helpers/routing-ops';
 import { getLink } from '@repo/data-ops/queries/links';
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
 import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
 import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
+
+App.get('/click-socket', async (c) => {
+	const upgradeHeader = c.req.header('Upgrade');
+	if (!upgradeHeader || upgradeHeader !== 'websocket') {
+		return c.text('Expected Upgrade: websocket', 426);
+	}
+
+	const accountId = c.req.header('account-id');
+	if (!accountId) return c.text('No Headers', 404);
+	const doId = c.env.LINK_CLICK_TRACKER_OBJECT.idFromName(accountId);
+	const stub = c.env.LINK_CLICK_TRACKER_OBJECT.get(doId);
+	return await stub.fetch(c.req.raw);
+});
 
 App.get('/:id', async (c) => {
 	const id = c.req.param('id');
@@ -33,6 +46,6 @@ App.get('/:id', async (c) => {
 			timestamp: new Date().toISOString(),
 		},
 	};
-	c.executionCtx.waitUntil(c.env.QUEUE.send(queueMessage));
+	c.executionCtx.waitUntil(captureLinkClickInBackground(c.env, queueMessage));
 	return c.redirect(destination);
 });
